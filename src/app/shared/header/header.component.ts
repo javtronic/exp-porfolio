b6a47d6899b6ccad5d48af763614f08cb5aab9b4
javtronic <juniorjavi7@gmail.com>
2023-11-10 18:07:57 -0500

    seccion 4 fin

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  public _servicio = inject(InfoPaginaService);
+
+  // constructor(public _servicio: InfoPaginaService){}
 
 }
